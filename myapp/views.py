4ca0b99c80dcd21b950c97d13a5595fc2708eaea
Tahajib Jakir Khan <jakir15-14503@diu.edu.bd>
2022-11-12 10:06:47 +0600

    tahajib1

---
+++ myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import Contact
+
+
+def dashboard(request):
+   return render(request,'home.html')
+def home(request):
+   return render(request,'home.html')
+def blog(request):
+   return render(request,'blog.html')
+def about(request):
+   return render(request,'about.html')
+def contact(request):
+   if request.method=="POST":
+      name=request.POST['name']
+      email=request.POST['email']
+      description=request.POST['description']
+      values=Contact(name=name, email=email, description=description)
+      values.save()
+
+   return render(request,'contact.html')
+    
+
+
